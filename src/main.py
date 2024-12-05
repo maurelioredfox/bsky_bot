295d7eed8f3358d750878b6768bf0d9818ca455a
maurelioredfox <aureliomarcos777@gmail.com>
2024-12-04 21:59:05 -0300

    fix: rabbit credentials

--- src/main.py
+++ src/main.py
@@ -2,6 +2,11 @@ import pika
 import json
 from api.bsky_api import handle_event, Event
 
+import os
+
+rabbit_user = os.environ.get('RABBIT_USER')
+rabbit_password = os.environ.get('RABBIT_PASSWORD')
+
 def callback(ch, method, properties, body):
     try:
         event =  Event(**json.loads(body))
@@ -12,7 +17,7 @@ def callback(ch, method, properties, body):
         ch.basic_ack(delivery_tag=method.delivery_tag) # so it doesn't get stuck in the queue
 
 def main():
-    credentials = pika.PlainCredentials('admin', 'admin')
+    credentials = pika.PlainCredentials(rabbit_user, rabbit_password)
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', credentials=credentials))
     channel = connection.channel()
 

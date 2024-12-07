82a946dcb5a6f91080c42148f64c5aa36d31ad88
maurelioredfox <aureliomarcos777@gmail.com>
2024-12-06 23:17:32 -0300

    added reply and delete to posts, fixed image ratio

--- src/dal/db.py
+++ src/dal/db.py
@@ -1,4 +1,4 @@
-from mongoengine import connect, Document, StringField
+from mongoengine import connect, Document, StringField, ReferenceField, SequenceField
 import os
 
 connect(os.getenv('TELEGRAM_DATABASE'), host=os.getenv('MONGO_HOST'), port=int(os.getenv('MONGO_PORT')))
@@ -6,3 +6,12 @@ connect(os.getenv('TELEGRAM_DATABASE'), host=os.getenv('MONGO_HOST'), port=int(o
 class Config(Document):
     Key  = StringField()
     Value = StringField()
+
+class Posts(Document):
+    id = SequenceField(primary_key=True)
+    text = StringField()
+    cid = StringField()
+    uri = StringField()
+    #refs to other Posts
+    parent = ReferenceField('self')
+    root = ReferenceField('self')
--- src/telegram_modules/bluesky.py
+++ src/telegram_modules/bluesky.py
@@ -2,7 +2,6 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import CommandHandler, ContextTypes, ConversationHandler, MessageHandler, filters, CallbackQueryHandler,Application
 
-import json
 import os
 import base64
 
@@ -33,6 +32,8 @@ async def set_authorized_user(update: Update, _: ContextTypes.DEFAULT_TYPE) -> N
     newConfig.save()
 
     await update.message.reply_text(f'Authorized user set to {user_id}')
+
+# region post
 
 async def bsky_post(update: Update, _: ContextTypes.DEFAULT_TYPE) -> int:
     user_id = update.effective_user.id
@@ -70,10 +71,95 @@ async def bsky_post_send(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     event.data = EventData()
     event.data.text = text
     event.data.image = image_base64
-    handle_event(event)
+    await handle_event(event)
 
     await update.message.reply_text('Post sent')
     return ConversationHandler.END
+
+# endregion
+
+# region list posts
+
+async def list_posts(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
+    user_id = update.effective_user.id
+    auth_config = db.Config.objects(Key = 'AuthorizedUser').first()
+    if not auth_config or auth_config.Value != str(user_id):
+        await update.message.reply_text('You are not authorized to use this command, your id is ' + str(user_id))
+        return
+    
+    event = Event()
+    event.eventType = EventType.List
+    event.data = EventData()
+    event.data.id = user_id
+    await update.message.reply_text('Fetching posts...')
+    await handle_event(event) 
+
+# endregion
+
+# region reply post
+
+async def reply_to_post(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_id = update.effective_user.id
+    auth_config = db.Config.objects(Key = 'AuthorizedUser').first()
+    if not auth_config or auth_config.Value != str(user_id):
+        await update.message.reply_text('You are not authorized to use this command, your id is ' + str(user_id))
+        return
+
+    if len(update.message.text.split('_')) < 2:
+        await update.message.reply_text('Please, use the link provided by the list_posts command')
+        return
+    
+    post_id = int(update.message.text.split('_')[-1])
+    context.user_data['post_id'] = post_id
+    await update.message.reply_text('Please, provide the text for the reply')
+    return STATE_POST
+
+async def send_reply(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_id = update.effective_user.id
+    auth_config = db.Config.objects(Key = 'AuthorizedUser').first()
+    if not auth_config or auth_config.Value != str(user_id):
+        await update.message.reply_text('You are not authorized to use this command, your id is ' + str(user_id))
+        return
+
+    text = update.message.text
+    post_id = context.user_data['post_id']
+
+    event = Event()
+    event.eventType = EventType.Reply
+    event.data = EventData()
+    event.data.text = text
+    event.data.id = post_id
+    await handle_event(event)
+    await update.message.reply_text('Reply sent')
+    return ConversationHandler.END
+
+# endregion
+
+# region delete post
+
+async def delete_post(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
+    user_id = update.effective_user.id
+    auth_config = db.Config.objects(Key = 'AuthorizedUser').first()
+    if not auth_config or auth_config.Value != str(user_id):
+        await update.message.reply_text('You are not authorized to use this command, your id is ' + str(user_id))
+        return
+
+    if len(update.message.text.split('_')) < 2:
+        await update.message.reply_text('Please, use the link provided by the list_posts command')
+        return
+    
+    post_id = int(update.message.text.split('_')[-1])
+
+    event = Event()
+    event.eventType = EventType.Delete
+    event.data = EventData()
+    event.data.id = post_id
+    await handle_event(event)
+    await update.message.reply_text('Post deleted, probably')
+
+# endregion
+
+# region update profile
 
 async def update_profile(update: Update, _: ContextTypes.DEFAULT_TYPE) -> int:
     user_id = update.effective_user.id
@@ -120,11 +206,6 @@ async def send_update(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int
     event = Event()
     event.eventType = EventType.Profile_Update
     event.data = EventData()
-    event.data.text = None
-    event.data.name = None
-    event.data.description = None
-    event.data.image = None
-    event.data.banner = None
     
     if update_type == 'name':
         event.data.name = text
@@ -135,10 +216,11 @@ async def send_update(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int
     elif update_type == 'banner':
         event.data.banner = image_base64
 
-    handle_event(event)
-
     await update.message.reply_text('Update sent')
+    await handle_event(event)
     return ConversationHandler.END
+
+# endregion
 
 async def stop(update: Update, _: ContextTypes.DEFAULT_TYPE) -> int:
     await update.message.reply_text('Operation cancelled')
@@ -160,6 +242,19 @@ def load(app: Application) -> None:
         fallbacks=[CommandHandler("stop", stop)]
     )
     app.add_handler(post_handler)
+
+    app.add_handler(CommandHandler("list_posts", list_posts))
+
+    reply_post_handler = ConversationHandler(
+        entry_points=[MessageHandler(filters.Regex('^/reply_[0-9]+$'), reply_to_post)],
+        states={
+            STATE_POST: [MessageHandler(filters.TEXT & ~filters.COMMAND, send_reply)]
+        },
+        fallbacks=[CommandHandler("stop", stop)]
+    )
+    app.add_handler(reply_post_handler)
+
+    app.add_handler(MessageHandler(filters.Regex('^/delete_[0-9]+$'), delete_post))
 
     update_profile_handler = ConversationHandler(
         entry_points=[CommandHandler("update_profile", update_profile)],
